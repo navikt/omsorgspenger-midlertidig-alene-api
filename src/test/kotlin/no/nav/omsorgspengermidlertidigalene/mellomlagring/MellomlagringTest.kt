748143360342fd3922b48d15a0ba7c1cb251fd9e
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-02-17 13:10:49 +0100

    Utvider mellomlagring til å tilby put. Dette oppdaterer mellomlagring, men øker ikke expire

--- src/test/kotlin/no/nav/omsorgspengermidlertidigalene/mellomlagring/MellomlagringTest.kt
+++ src/test/kotlin/no/nav/omsorgspengermidlertidigalene/mellomlagring/MellomlagringTest.kt
@@ -9,15 +9,18 @@ import no.nav.omsorgspengermidlertidigalene.TestConfiguration
 import no.nav.omsorgspengermidlertidigalene.redis.RedisConfig
 import no.nav.omsorgspengermidlertidigalene.redis.RedisMockUtil
 import no.nav.omsorgspengermidlertidigalene.redis.RedisStore
+import org.awaitility.Awaitility
+import org.awaitility.Durations
 import org.junit.AfterClass
-import kotlin.test.Test
-import kotlin.test.assertEquals
-import kotlin.test.assertNotEquals
-import kotlin.test.assertNotNull
+import org.slf4j.LoggerFactory
+import java.util.*
+import kotlin.test.*
 
 @KtorExperimentalAPI
 class MellomlagringTest {
     private companion object {
+        val logger = LoggerFactory.getLogger(MellomlagringTest::class.java)
+
         val redisServer: RedisServer = RedisServer
             .newRedisServer(6379)
             .started()
@@ -62,6 +65,59 @@ class MellomlagringTest {
         val mellomlagring = mellomlagringService.getMellomlagring("test")
         assertNotNull(redisStore.get("mellomlagring_test"))
         assertNotEquals(mellomlagring, redisStore.get("test"))
+    }
+
+    @Test
+    internal fun `Oppdatering av mellomlagret verdi, skal ikke slette expiry`() {
+        val key = "test"
+        val expirationDate = Calendar.getInstance().let {
+            it.add(Calendar.MINUTE, 1)
+            it.time
+        }
+
+        mellomlagringService.setMellomlagring(
+            fnr = key,
+            midlertidigSøknad = "test",
+            expirationDate = expirationDate
+        )
+        val verdi = mellomlagringService.getMellomlagring(key)
+        assertEquals("test", verdi)
+        val ttl = mellomlagringService.getTTLInMs(key)
+        assertNotEquals(ttl, "-2")
+        assertNotEquals(ttl, "-1")
+
+        logger.info("PTTL=$ttl")
+
+        mellomlagringService.updateMellomlagring(key, "test2")
+        val oppdatertVerdi = mellomlagringService.getMellomlagring(key)
+        assertEquals("test2", oppdatertVerdi)
+        assertNotEquals(ttl, "-2")
+        assertNotEquals(ttl, "-1")
+
+
+    }
+
+    @Test
+    internal fun `mellomlagret verdier skal være utgått etter 500 ms`() {
+        val fnr = "12345678910"
+        val søknad = "test"
+
+        val expirationDate = Calendar.getInstance().let {
+            it.add(Calendar.MILLISECOND, 500)
+            it.time
+        }
+        mellomlagringService.setMellomlagring(fnr, søknad, expirationDate = expirationDate)
+        val forventetVerdi1 = mellomlagringService.getMellomlagring(fnr)
+        logger.info("Hentet mellomlagret verdi = {}", forventetVerdi1)
+        assertEquals("test", forventetVerdi1)
+        assertNotEquals(mellomlagringService.getTTLInMs(fnr), "-2")
+        assertNotEquals(mellomlagringService.getTTLInMs(fnr), "-1")
+
+        Awaitility.waitAtMost(Durations.ONE_SECOND).untilAsserted {
+            val forventetVerdi2 = mellomlagringService.getMellomlagring(fnr)
+            logger.info("Hentet mellomlagret verdi = {}", forventetVerdi2)
+            assertNull(forventetVerdi2)
+        }
     }
 
 }
