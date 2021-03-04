3ce728b74cc01ae86b6dc913a8a37d84dc1b6e6a
Sander Opperud Odden <sander.opperud.odden@nav.no>
2021-03-04 09:49:39 +0100

    Fjerner utgåtte felter. Setter på validering. Oppdaterer readme

--- src/main/kotlin/no/nav/omsorgspengermidlertidigalene/søknad/søknad/Søknad.kt
+++ src/main/kotlin/no/nav/omsorgspengermidlertidigalene/søknad/søknad/Søknad.kt
@@ -1,6 +1,5 @@
 package no.nav.omsorgspengermidlertidigalene.søknad.søknad
 
-import com.fasterxml.jackson.annotation.JsonAlias
 import no.nav.omsorgspengermidlertidigalene.barn.BarnOppslag
 import no.nav.omsorgspengermidlertidigalene.søker.Søker
 import java.time.ZoneOffset
@@ -11,11 +10,7 @@ data class Søknad(
     val søknadId: String = UUID.randomUUID().toString(),
     val id: String,
     val språk: String,
-    val arbeidssituasjon: List<Arbeidssituasjon>? = null, //TODO 26.02.2021 - Fjernes når frontend er prodsatt
     val annenForelder: AnnenForelder,
-    val antallBarn: Int? = null, //TODO 26.02.2021 - Fjernes når frontend er prodsatt
-    val fødselsårBarn: List<Int>? = null, //TODO 26.02.2021 - Fjernes når frontend er prodsatt
-    val medlemskap: Medlemskap? = null, //TODO 26.02.2021 - Fjernes når frontend er prodsatt
     val barn: List<Barn> = listOf(),
     val harForståttRettigheterOgPlikter: Boolean,
     val harBekreftetOpplysninger: Boolean
@@ -26,11 +21,7 @@ data class Søknad(
             søknadId = søknadId,
             id = id,
             språk = språk,
-            arbeidssituasjon = arbeidssituasjon,
             annenForelder = annenForelder,
-            antallBarn = antallBarn,
-            fødselsårBarn = fødselsårBarn,
-            medlemskap = medlemskap,
             barn = barn,
             harBekreftetOpplysninger = harBekreftetOpplysninger,
             harForståttRettigheterOgPlikter = harForståttRettigheterOgPlikter
@@ -43,13 +34,6 @@ data class Søknad(
             }
         }
     }
-}
-
-enum class Arbeidssituasjon(){
-    @JsonAlias("selvstendigNæringsdrivende") SELVSTENDIG_NÆRINGSDRIVENDE,
-    @JsonAlias("arbeidstaker") ARBEIDSTAKER,
-    @JsonAlias("frilanser") FRILANSER,
-    @JsonAlias("annen") ANNEN
 }
 
 private fun List<BarnOppslag>.hentIdentitetsnummerForBarn(aktørId: String?): String? {
